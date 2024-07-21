25f6e9bfa0b252136a6f4590a8f59c695a804bb4
ddung203 <duongvandung2k3@gmail.com>
2024-07-22 01:59:14 +0700

    feat: set requestCount every 15min

--- src/configs/requestLogger.ts
+++ src/configs/requestLogger.ts
@@ -3,6 +3,10 @@ import logger from "./logger";
 
 let requestCount = 0;
 
+setInterval(() => {
+  requestCount = 0;
+}, 900000);
+
 const requestLogger = (req: Request, res: Response, next: NextFunction) => {
   requestCount++;
   logger.info(`Request #${requestCount}: ${req.method} ${req.originalUrl}`);
